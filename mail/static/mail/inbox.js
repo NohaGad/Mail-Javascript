a45a03fed113754a1ec3299f391460f01d859c6d
nohaahed <nohaahed209@gmail.com>
2023-11-02 22:14:48 +0100

    Create sent_mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,14 +1,45 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit', (event) => {
+
+    const recipients = document.querySelector('#compose-recipients').value
+    const subject = document.querySelector('#compose-subject').value
+    const body = document.querySelector('#compose-body').value
+
+    send_email(recipients, subject, body);
+    alert("noha")
+    load_mailbox('sent');
+    event.preventDefault();
+
+  })
 
   // By default, load the inbox
   load_mailbox('inbox');
 });
+
+function send_email(recipients, subject, body) {
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+    })
+  })
+    .then(response => response.json())
+    .then(result => {
+      // Print result
+      console.log(result);
+
+    });
+
+}
 
 function compose_email() {
 
@@ -23,7 +54,7 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
